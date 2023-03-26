b555bff5a610c165a843ad9c0dd0129f3a23d484
TheoZerbibi <tzeribi@gmail.com>
2023-03-26 22:38:57 +0200

    Add Migration for Ban table

---
+++ migrations/sql/1679862959928-CreateBanTable.sql
@@ -0,0 +1,22 @@
+/* CreateBanTable
+
+You can write your SQL migration here.
+Please be sure that your migration work before pushing them to git.
+You can run your migration by using DatabaseMigration artifact and running
+`java -jar DatabaseMigration.jar migrate`
+*/
+
+CreateBanTable {
+create table ban
+(
+    id         int auto_increment,
+    author     varchar(255)                        not null,
+    target     varchar(255)                        not null,
+    reason     varchar(255)                        not null,
+    end_at     timestamp                           not null,
+    created_at timestamp default CURRENT_TIMESTAMP null,
+    updated_at timestamp default CURRENT_TIMESTAMP null,
+    constraint ban_pk
+        primary key (id)
+);
+}
