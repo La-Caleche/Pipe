bedab3cb2956509d60e1614e63c97f86a2ad00ef
TheoZerbibi <tzeribi@gmail.com>
2023-03-27 01:06:53 +0200

    modif migration

--- migrations/sql/1679862959928-CreateBanTable.sql
+++ migrations/sql/1679862959928-CreateBanTable.sql
@@ -7,16 +7,16 @@ You can run your migration by using DatabaseMigration artifact and running
 */
 
 CreateBanTable {
-create table bans
-(
-    id         int auto_increment,
-    author     varchar(255)                        not null,
-    target     varchar(255)                        not null,
-    reason     varchar(255)                        not null,
-    end_at     timestamp                           not null,
-    created_at timestamp default CURRENT_TIMESTAMP null,
-    updated_at timestamp default CURRENT_TIMESTAMP null,
-    constraint ban_pk
-        primary key (id)
-);
+    create table bans
+    (
+        id         int auto_increment,
+        author     varchar(255)                        not null,
+        target     varchar(255)                        not null,
+        reason     varchar(255)                        not null,
+        end_at     timestamp                           null,
+        created_at timestamp default CURRENT_TIMESTAMP null,
+        updated_at timestamp default CURRENT_TIMESTAMP null,
+        constraint ban_pk
+            primary key (id)
+    );
 }
